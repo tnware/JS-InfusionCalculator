a7ccbf869d0399160b4fdf8475eed7786f0d8b2b
Tyler Woods <tyler.woods66@gmail.com>
2019-05-09 20:42:44 -0500

    functional cost and updated aesthetic components

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ function bmi () {
     var bhopercent = Number(document.getElementById("bhopercent").value);
     var nugpercent = Number(document.getElementById("nugpercent").value);
     var capsize = Number(document.getElementById("capsize").value);
+    var bhocost = Number(document.getElementById("bhocost").value);
+    var nugcost = Number(document.getElementById("nugcost").value);
+    var oilcost = Number(document.getElementById("oilcost").value);
     var MGs_total = ((bhopercent * 10) * weightBHO) + ((nugpercent * 10) * weightNUG);
     var caps_total = (volumeOIL * 30) / capsize;
     var mgpercap = MGs_total / caps_total;
     var mltotal = volumeOIL * 30;
     var soylecithin = volumeOIL * 0.375;
     var soyfrac = new Fraction(soylecithin);
+    var costpercap = ((bhocost + nugcost + oilcost) / caps_total);
+    var costperml = ((bhocost + nugcost + oilcost) / mltotal);
     document.getElementById("soylecithin").innerHTML = "Optionally add " +  soyfrac.toString() + " tsp. of lecithin (soy/sunflower) during infusion process.";
     document.getElementById("MGs_total").innerHTML = "" + MGs_total + "mgs total";
     document.getElementById("caps_total").innerHTML = "" + Math.floor(caps_total) + " caps";
     document.getElementById("mgpercap").innerHTML = "" +  mgpercap + " mg/cap";
     document.getElementById("mltotal").innerHTML = "" +  mltotal + " mL";
+    document.getElementById("costpercap").innerHTML = "$" + costpercap + "/cap";
+    document.getElementById("costperml").innerHTML = "$" + costperml + "/mL";
     }
